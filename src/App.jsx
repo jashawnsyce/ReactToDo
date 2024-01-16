499be63085df15d9e036653049ae0c94cbd0e40a
Jay Syce <jaysycework@gmail.com>
2024-01-15 19:57:44 -0500

    seperated components

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { NewTodoForm } from './NewTodoForm'
 
 export default function App() {
-  const [newItem, setNewItem] = useState("boobs")
   const [todos, setTodos] = useState([])
-  
-  function handleSubmit(e) {
-    e.preventDefault()
 
-    if (newItem !== "") {
+  function addToDo(value) {
       setTodos(currTodos => {
-        return [...currTodos, {id: crypto.randomUUID(), value: newItem, }]
+        return [...currTodos, {id: crypto.randomUUID(), value, }]
           }
         ) 
-      setNewItem("")
-      }
   }
 
   function deleteTodo (id) {
@@ -28,27 +23,21 @@ export default function App() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className='new-item-form'>
-        <div className='form-row'>
-          <label htmlFor='item'>New Item</label>
-          <input value={ newItem } onChange={e =>setNewItem(e.target.value) } type='text' id='item'/>
-        </div>
-        <button className='btn'>Add</button>
-      </form>
-
-      <h1 className='header'>Todo list</h1>
-      <ul className='list'>
-        {todos.map(todo => {
-            return <li key={todo.id}>
-              <label>
-                <input type='checkbox'/> { todo.value }
-              </label>
-              <button onClick={() => deleteTodo(todo.id)} className=' btn btn-danger'> Delete</button>
-            </li>
+    <NewTodoForm onSubmit={addToDo}/>
+    <h1 className='header'>Todo list</h1>
+    <ul className='list'>
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input type='checkbox'/> { todo.value }
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} className=' btn btn-danger'> Delete</button>
+          </li>
+              )    
             }
           )
-        }
-        
+        }  
       </ul>
     </>    
   )
